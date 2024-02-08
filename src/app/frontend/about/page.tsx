0bffe3dc1937a9f4b2767c1ad79ec4e53b025c1f
Enzo G. Borrelli A <enzoborrellidev@gmail.com>
2024-02-08 20:29:52 -0300

    feat: complete the about section

--- src/app/frontend/about/page.tsx
+++ src/app/frontend/about/page.tsx
@@ -1,7 +1,114 @@
-import React from 'react'
+"use client";
+import {
+  aboutDivStyle,
+  mainStyle,
+} from "@/components/frontend/home/stylesConst";
+import useLang from "@/languages/useLang";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function About() {
+  const text = useLang();
+  if (!text) return <></>;
+
   return (
-    <div>About</div>
-  )
+    <main className={mainStyle}>
+      <div className="flex flex-col md:flex-row items-center justify-center w-full h-full">
+        <h1 className="text-xl text-center font-bold m-4">
+          {text.frontAboutH1_part1}
+          <br />
+          {text.frontAboutH1_part2} <br />
+          {text.frontAboutH1_part3}
+        </h1>
+        <Image
+          src="/images/yoMismo.webp"
+          width={200}
+          height={200}
+          alt="me"
+          loading="lazy"
+        />
+      </div>
+      <div className={aboutDivStyle}>
+        <h2 className="text-lg font-bold">{text.frontAboutH2_who}</h2>
+        <p className="text-md text-center">
+          {text.frontAbout_firstP1}
+          <br />
+          {text.frontAbout_firstP2}
+          <br />
+          {text.frontAbout_firstP3}
+          <br />
+          {text.frontAbout_firstP4}
+          <br />
+          {text.frontAbout_firstP5}
+          <br />
+        </p>
+      </div>
+      <div className={aboutDivStyle}>
+        <h2 className="text-lg font-bold">{text.frontAboutH2_moreAbout}</h2>
+        <p className="flex flex-col items-center text-center gap-2">
+          {text.frontAbout_MC_P}
+          <Image
+            src="/images/extras/mapmaking_2012.png"
+            width={300}
+            height={250}
+            alt="mapmaking"
+            loading="lazy"
+          />
+        </p>
+        <p className="flex flex-col items-center text-center gap-2">
+          {text.frontAbout_Clang}
+          <Image
+            src="/images/extras/arduinocc.png"
+            width={250}
+            height={250}
+            alt="arduino"
+            loading="lazy"
+          />
+        </p>
+        <p className="text-center flex flex-col items-center my-4 gap-2">
+          {text.frontAbout_firstHTML}
+          <a
+            className="bg-teal-500 w-fit p-2 rounded-md border-2 border-teal-800 text-black hover:animate-bounce"
+            href="https://enzoborrelli.github.io/informe-en-html/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {text.frontAbout_toProjBtn}
+          </a>
+        </p>
+        <p className="text-center flex flex-col items-center my-4 gap-2">
+          {text.frontAbout_reactProj}
+          <a
+            className="bg-teal-500 w-fit p-2 rounded-md border-2 border-teal-800 text-black hover:animate-bounce"
+            href="https://emrobots.com.ar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {text.frontAbout_to2ndProjBtn}
+          </a>
+        </p>
+        <p className="text-center">{text.frontAbout_freeTime}</p>
+      </div>
+      <div className={aboutDivStyle}>
+        <h2 className="text-lg font-bold">{text.frontAboutH2_random}:</h2>
+        <p className="text-center">{text.frontAbout_Sendo}</p>
+        <Image
+          src="/images/extras/sendo.png"
+          width={250}
+          height={250}
+          alt="logo"
+          loading="lazy"
+        />
+        <p className="flex flex-col items-center text-center">
+          {text.frontAbout_YTChannel}
+          <span className="bg-teal-500 w-fit p-2 rounded-md border-2 border-teal-800 text-black hover:animate-bounce">
+            <Link href="*/video/about" as="/video/about">
+              {text.frontAbout_toVideo}
+            </Link>
+          </span>
+        </p>
+      </div>
+    </main>
+  );
 }
--- src/components/frontend/home/stylesConst.tsx
+++ src/components/frontend/home/stylesConst.tsx
@@ -5,3 +5,5 @@ export const iconDivStyle = "flex flex-wrap justify-center text-3xl gap-3";
 export const iconLinkStyle = "shadow-md p-1";
 
 export const mainStyle = "bg-gray-200 w-full h-full flex flex-col p-2 items-center justify-center text-slate-600"
+
+export const aboutDivStyle = "flex flex-col items-center gap-3 justify-center w-full h-full my-4"
