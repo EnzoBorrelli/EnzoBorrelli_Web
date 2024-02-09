c33ddc9592a3411c3555ffd986a989b21b819ff2
Enzo G. Borrelli A <enzoborrellidev@gmail.com>
2024-02-09 17:47:50 -0300

    feat: videos page almost done

--- src/app/video/videos/page.tsx
+++ src/app/video/videos/page.tsx
@@ -1,25 +1,215 @@
-import { mainStyle } from "@/components/stylesConst";
+import {
+  H2Style,
+  ImageStyle,
+  mainStyle,
+  videoLandscapeDivStyle,
+  videoPortraitDivStyle,
+} from "@/components/stylesConst";
+import Image from "next/image";
 import React from "react";
+
+const enYTvideos = [
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/youtube/placeholder.webp",
+    url: "placeholder",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/youtube/placeholder.webp",
+    url: "placeholder",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/youtube/placeholder.webp",
+    url: "placeholder",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/youtube/placeholder.webp",
+    url: "placeholder",
+  },
+];
+const tiktokVideos = [
+  {
+    key: "BBTiktok",
+    image: "/images/video-thumbnails/tiktok/black_tiktok.webp",
+    url: "https://www.tiktok.com/@enderhell24/video/7325561196994628869",
+  },
+  {
+    key: "gojotiktok",
+    image: "/images/video-thumbnails/tiktok/gojo_tiktok.webp",
+    url: "https://www.tiktok.com/@enderhell24/video/7325913885863578886",
+  },
+  {
+    key: "sukunaTiktok",
+    image: "/images/video-thumbnails/tiktok/sukuna_tiktok.webp",
+    url: "https://www.tiktok.com/@enderhell24/video/7329238339779906822",
+  },
+  {
+    key: "gjinxTiktok",
+    image: "/images/video-thumbnails/tiktok/jinx_tiktok.webp",
+    url: "https://www.tiktok.com/@enderhell24/video/7330316626732076293",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.tiktok.com/@enderhell24",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.tiktok.com/@enderhell24",
+  },
+];
+const igVideos = [
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+  {
+    key: "placeholder",
+    image: "/images/video-thumbnails/tiktok/placeholder_vertical.webp",
+    url: "https://www.instagram.com/ender_devlogs/",
+  },
+];
+const YTvideos = [
+  {
+    key: "ctrVideo",
+    image: "/images/video-thumbnails/youtube/ctr_video.webp",
+    url: "https://www.youtube.com/watch?v=rbpxX9wkHCo",
+  },
+  {
+    key: "f9Video",
+    image: "/images/video-thumbnails/youtube/f9_video.webp",
+    url: "https://www.youtube.com/watch?v=Fj0kxx2d41U",
+  },
+  {
+    key: "fortniteVideo",
+    image: "/images/video-thumbnails/youtube/fortnite_video.webp",
+    url: "https://www.youtube.com/watch?v=ZCKvthIhF5w",
+  },
+  {
+    key: "genshinVideo",
+    image: "/images/video-thumbnails/youtube/genshin_video.webp",
+    url: "https://www.youtube.com/watch?v=6rOPkIlBtZM",
+  },
+];
 
 export default function Projects() {
   return (
-    <main className={mainStyle}>
-      <h1>'title'</h1>
+    <main className={`${mainStyle} gap-6`}>
+      <h1 className="text-3xl font-bold mt-1 text-center">
+        Content made by Me
+      </h1>
       <section>
-        <h2>'section title'</h2>
-        <div>'carrousel of videos'</div>
+        <h2 className={H2Style}>Youtube: template(Endy the programmer)</h2>
+        <div className={videoLandscapeDivStyle}>
+          {enYTvideos.map((video) => (
+            <a
+              key={video.key}
+              href={video.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className={ImageStyle}
+                src={video.image}
+                width={320}
+                height={180}
+                alt={video.key}
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </div>
       </section>
       <section>
-        <h2>'section title'</h2>
-        <div>'carrousel of videos'</div>
+        <h2 className={H2Style}>Tiktok: @EnderHell24</h2>
+        <div className={videoPortraitDivStyle}>
+          {tiktokVideos.map((video) => (
+            <a
+              key={video.key}
+              href={video.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className={ImageStyle}
+                src={video.image}
+                width={180}
+                height={320}
+                alt={video.key}
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </div>
       </section>
       <section>
-        <h2>'section title'</h2>
-        <div>'carrousel of videos'</div>
+        <h2 className={H2Style}>Instagram: Ender_Devlogs</h2>
+        <div className={videoPortraitDivStyle}>
+          {igVideos.map((video) => (
+            <a
+              key={video.key}
+              href={video.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className={ImageStyle}
+                src={video.image}
+                width={180}
+                height={320}
+                alt={video.key}
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </div>
       </section>
       <section>
-        <h2>'section title'</h2>
-        <div>'carrousel of videos'</div>
+        <h2 className={H2Style}>Youtube: the relics of Ender</h2>
+        <div className={videoLandscapeDivStyle}>
+          {YTvideos.map((video) => (
+            <a
+              key={video.key}
+              href={video.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className={ImageStyle}
+                src={video.image}
+                width={320}
+                height={180}
+                alt={video.key}
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </div>
       </section>
     </main>
   );
--- src/components/stylesConst.ts
+++ src/components/stylesConst.ts
@@ -1,15 +1,24 @@
 // general styles
-export const mainStyle = "bg-gray-200 w-full h-full flex flex-col p-2 items-center justify-center text-slate-600"
+export const mainStyle =
+  "bg-gray-200 w-full h-full flex flex-col p-2 items-center justify-center text-slate-600";
 
-export const aboutDivStyle = "flex flex-col items-center gap-3 justify-center w-full h-full my-4"
-
+export const aboutDivStyle =
+  "flex flex-col items-center gap-3 justify-center w-full h-full my-4";
 
 // styles used in the frontend section
-export const sectionsStyles ="w-full flex flex-col items-center justify-center md:flex-row my-3";
+export const sectionsStyles =
+  "w-full flex flex-col items-center justify-center md:flex-row my-3";
 
 export const iconDivStyle = "flex flex-wrap justify-center text-3xl gap-3";
 
 export const iconLinkStyle = "shadow-md p-1";
 
 //styles used in the video section
-export const aboutBtnStyle= "bg-gradient-to-t from-rose-400 to-pink-300 w-fit p-1 rounded-md border-2 border-pink-800 text-black hover:animate-bounce"
+export const aboutBtnStyle =
+  "bg-gradient-to-t from-rose-400 to-pink-300 w-fit p-1 rounded-md border-2 border-pink-800 text-black hover:animate-bounce";
+export const H2Style = "text-xl font-bold mb-2 text-center md:text-left";
+export const videoPortraitDivStyle =
+  "grid place-items-center grid-cols-2 sm:grid-cols-3 md:grid-cols-6 grid-rows-3 sm:grid-rows-2 md:grid-rows-1 gap-2";
+export const videoLandscapeDivStyle =
+  "grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 grid-rows-4 sm:grid-rows-2 md:grid-rows-1 gap-2";
+export const ImageStyle = "rounded-lg border-2 border-slate-400";
